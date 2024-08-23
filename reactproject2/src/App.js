380142dd4a3c05635d8258093350ed5aa9359d27
chaalan <chaalan2004@gmail.com>
2024-08-23 11:51:51 +0300

    create random color generator

---
+++ reactproject2/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ColorGenerator from "./RandomColorGenerator";
+
+function App() {
+  return (
+    <div className="App">
+      <ColorGenerator />
+    </div>
+  );
+}
+
+export default App;
